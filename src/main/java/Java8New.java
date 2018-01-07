45f5d73badb438761592bee33f5229d28608a2a9
admin <admin@DESKTOP-307SKFB>
2018-01-08 00:05:38 +0530

    JavaFeature8

--- src/main/java/Java8New.java
+++ src/main/java/Java8New.java
@@ -1,62 +1,101 @@
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Stream;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 public class Java8New {
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy");
+	private static BigDecimal rsval  =BigDecimal.ZERO;
 
-	public static void main(String arg[]) {
+	public static boolean filteronDate(Map<String, String> date, String reportDate)  {
+		{
+			try {
+				
+				return (sdf.parse(date.get("ReportDate")).compareTo(sdf.parse(reportDate)) > 0);
+			} catch (ParseException e) {
+				e.printStackTrace();
+				return false;
+			}
 
-		final int i = 0;
-		Map<String, String> str1 = new HashMap<String, String>();
-		str1.put("ReportDate", "31-Aug-2018");
-		str1.put("Value", "200");
+		}
+	}
+	
+	public static BigDecimal filteronValue(String val) {
+		{
+			if (!val.equalsIgnoreCase("null")) {
+				return new BigDecimal(val);
+			}
+			return BigDecimal.ZERO;
+		}
+	}
+	
+	/*public static void  result(BigDecimal val) {
+		
+		rsval.add(val);
+	}*/
+	
+	public static void main(String arg[]) throws ParseException {
 
-		Map<String, String> str2 = new HashMap<String, String>();
-		str2.put("ReportDate", "29-Sep-2018");
-		str2.put("Value", "150");
+		
+
+		Map<String, String> str4 = new HashMap<String, String>();
+		str4.put("ReportDate", "31-Aug-2018");
+		str4.put("Value", "200");
 
 		Map<String, String> str3 = new HashMap<String, String>();
 		str3.put("ReportDate", "30-Sep-2018");
 		str3.put("Value", "150");
 
+		Map<String, String> str2 = new HashMap<String, String>();
+		str2.put("ReportDate", "31-Oct-2018");
+		str2.put("Value", "null");
+
+		Map<String, String> str1 = new HashMap<String, String>();
+		str1.put("ReportDate", "30-Nov-2018");
+		str1.put("Value", "250");
+
+		Map<String, String> str5 = new HashMap<String, String>();
+		str5.put("ReportDate", "31-Dec-2018");
+		str5.put("Value", "350");
+
 		List<Map<String, String>> numberList = new ArrayList<Map<String, String>>();
-		numberList.add(str1);
-		numberList.add(str2);
+		numberList.add(str4);
 		numberList.add(str3);
+		numberList.add(str2);
+		numberList.add(str1);
+		numberList.add(str5);
 
-		AtomicInteger atomicInteger = new AtomicInteger(0);
+		System.out.println(sdf.parse("30-Sep-2018"));
 
-		/*
-		 * int filterNumber = numberList.stream() .filter(number -> number==5)
-		 * .collect(Collectors.toList()).indexOf(5);
-		 */
-
+		List<BigDecimal> filterNumber = numberList.stream().filter(number -> filteronDate(number,"30-Sep-2018")
+		).limit(3).map(ele -> filteronValue(ele.get("Value"))).collect(Collectors.toList());
+		filterNumber.forEach(rs->rsval=rsval.add(rs));
+		
+		//numberList.stream().filter(number -> filteronDate(number,"30-Sep-2018")
+		//).limit(3).map(ele -> filteronValue(ele.get("Value"))).forEach(System.out::println);
+	
+		System.out.println(rsval);
+		
 		// filterNumber.forEach(System.out::println);
 
-	/*	Stream<Map<String, String>> lib = numberList.stream()
-				.filter(number -> number.get("ReportDate").equalsIgnoreCase("30-Sep-2018"));
-				
-
-		 numberList.forEach(number ->{
-		 
-			
-		 atomicInteger.getAndIncrement(); });*/
-		 
-		System.out.println(atomicInteger.get());
-
 		/*
-		 * Stream<Object> index = numberList.stream().map(n -> {
+		 * Stream<Map<String, String>> lib = numberList.stream() .filter(number
+		 * -> number.get("ReportDate").equalsIgnoreCase("30-Sep-2018"));
 		 * 
 		 * 
-		 * if(n.get("ReportDate").equalsIgnoreCase("31-Aug-2018")){ return
-		 * atomicInteger; }
+		 * numberList.forEach(number ->{
 		 * 
-		 * });
+		 * 
+		 * atomicInteger.getAndIncrement(); });
 		 */
+
+		
 
 	}
 
